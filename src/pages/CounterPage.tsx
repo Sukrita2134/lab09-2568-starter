6bbfb9d5faff34b0e2063dcd3fa121854007bd95
Sukrtia2134 <sugritabursuk@gmail.com>
2025-08-05 03:01:20 +0700

    finished

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function CounterPage() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
-      {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      <p>Counter: {count}</p>
+      <button className="btn btn-success" onClick={increase}>
+        Increase
+      </button>
     </div>
   );
 }
--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function HelloMyNamePage() {
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
+
+  const greet = () => {
+    setGreeting(`Hello ${name}`);
+  };
+
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +15,14 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary" onClick={greet}>
+        Greet Me
+      </button>
       {/* Result Text */}
-      <p></p>
+      <p className="mt-3">{greeting}</p>
     </div>
   );
 }
